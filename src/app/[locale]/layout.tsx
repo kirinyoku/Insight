60f807ed19e1f8e9178e8d843203495f81a064a3
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-05 14:44:41 +0300

    Init i18n

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,56 @@
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import { notFound } from "next/navigation";
+import { NextIntlClientProvider } from "next-intl";
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+const locales = ["en", "ua"];
+
+export const metadata: Metadata = {
+  title: "Insight",
+  description: "Talk with your documents.",
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+  params: {
+    locale: "en" | "ua";
+  };
+}
+
+export default async function RootLayout({
+  children,
+  params: { locale },
+}: RootLayoutProps) {
+  // Validate that the incoming `locale` parameter is valid.
+  const isValidLocale = locales.some((l) => l === locale);
+  if (!isValidLocale) {
+    notFound();
+  }
+
+  // Import and validation i18n messages for client components.
+  let messages;
+  try {
+    messages = (await import(`@/i18n/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale}>
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased grainy",
+          inter.className
+        )}
+      >
+        {/* NextIntlClientProvider for i18n client components */}
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
