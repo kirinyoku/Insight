0104231d6782fe8e66de0d0424293e50fb5b63c1
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-07 13:44:29 +0300

    Language changes are organized into one component

---
+++ src/components/LanguageSwitch.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import Image from "next/image";
+import {
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "./ui/menubar";
+import { useLocale } from "next-intl";
+
+// LanguageSwitch component can only be used in the parent Menubar component
+export default function LanguageSwitch() {
+  const locale = useLocale();
+  const language = locale === "ua" ? "Українська" : "English";
+
+  return (
+    <MenubarMenu>
+      <MenubarTrigger className="cursor-pointer flex gap-1">
+        <Image
+          src={
+            locale === "ua"
+              ? "ukraine-flag-icon.svg"
+              : "united-kingdom-flag-icon.svg"
+          }
+          width={15}
+          height={15}
+          alt={`${language}`}
+        />
+        {language}
+      </MenubarTrigger>
+      <MenubarContent className="min-w-fit">
+        <MenubarItem>
+          <Link href="/en" className="w-full flex gap-1">
+            <Image
+              src="/united-kingdom-flag-icon.svg"
+              width={15}
+              height={15}
+              alt="united kingdom flag"
+            />
+            English
+          </Link>
+        </MenubarItem>
+        <MenubarItem>
+          <Link href="/ua" className="w-full flex gap-1">
+            <Image
+              src="/ukraine-flag-icon.svg"
+              width={15}
+              height={15}
+              alt="ukraine flag"
+            />
+            Українська
+          </Link>
+        </MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+  );
+}
