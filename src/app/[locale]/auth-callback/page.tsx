9542bc994aa9b28b1f3e1f1f660fa53b69063f0e
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-09 12:14:56 +0300

    Added auth callback page
    The authorization callback page is used to synchronize the user with the database.

---
+++ src/app/[locale]/auth-callback/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Loader2 } from "lucide-react";
+import { trpc } from "@/app/_trpc/client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useTranslations } from "next-intl";
+
+export default function AuthCallbackPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
+
+  const i18n = useTranslations("AuthCallback");
+
+  trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        router.push(origin ? `/${origin}` : "/dashboard");
+      }
+    },
+    onError: (error) => {
+      if (error.data?.code === "UNAUTHORIZED") {
+        router.push("/sign-in");
+      }
+    },
+    retry: true,
+    retryDelay: 500,
+  });
+
+  return (
+    <main className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-foreground/90" />
+        <h2 className="font-semibold text-xl">{i18n("info")}</h2>
+        <p>{i18n("warning")}</p>
+      </div>
+    </main>
+  );
+}
