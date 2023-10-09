964b622f0e8f325c87af56b83f1f9a1e5b88907e
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-09 12:28:09 +0300

    Added Dashboard base page

---
+++ src/app/[locale]/dashboard/page.tsx
@@ -0,0 +1,28 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  /*
+    The origin query is needed to redirect the user 
+    back to this page after authorization.
+  */
+  if (!user || !user?.id) redirect("/auth-callback?origin=dashboard");
+
+  // get user from DB
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  // if the user doesn't exist in DB, redirect to the AuthCallback page
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
+
+  return {
+    /* Dashboard Component */
+  };
+}
