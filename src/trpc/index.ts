7c24f49d86d8e9000614b320140c32a1350940ee
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-10 11:26:05 +0300

    Added tRPC procedures
    userFiles: private procedure for querying all user files;
    deleteFile: private procedure for deleting user file.

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,8 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { publicProcedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
+import { z } from "zod";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -31,9 +32,38 @@ export const appRouter = router({
 
     return { success: true };
   }),
-  test: publicProcedure.query(() => {
-    return { message: "Hello World" };
+  userFiles: privateProcedure.query(async ({ ctx }) => {
+    const { user, userId } = ctx;
+
+    return await db.file.findMany({
+      where: {
+        userId: userId,
+      },
+    });
   }),
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      // only its author can delete a file
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId: userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
