c4ccb95da21f591575602f2df20e2ea1ea458185
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-12 14:46:02 +0300

    Added UI components from shadcn lib

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
 const withNextIntl = require("next-intl/plugin")("./i18n.ts");
 
 module.exports = withNextIntl(nextConfig);
