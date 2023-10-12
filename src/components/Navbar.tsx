7b3d5be6307450f7cda2eca416d94a3739ae5f01
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-12 09:49:01 +0300

    Updated Navbar component
    When a user is logged in, they see the dashbaord and log-out buttons.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,22 @@ import ThemeSwitch from "./ThemeSwitch";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "./ui/button";
 import { Menubar } from "./ui/menubar";
-import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 import { useTranslations } from "next-intl";
 
 export default function Navbar() {
   const i18n = useTranslations("Index");
 
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
   return (
-    <nav className="sticky inset-x-0 top-0 z-50 w-full border-b border-b-border h-14 bg-background/60 backdrop-blur-lg transition-all">
+    <nav className="sticky inset-x-0 top-0 z-50 w-full border-b border-b-border h-14 bg-background/50 backdrop-blur-lg shadow-primary/10 shadow-lg transition-all">
       <Container>
         <div className="flex h-14 items-center justify-between border-b border-border">
           <Link
@@ -27,21 +35,37 @@ export default function Navbar() {
             </span>
           </Link>
           <div className="flex gap-10 items-center">
-            <Menubar>
+            <Menubar className="bg-background/50">
               {/* LanguageSwitch and ThemeSwitch return ManubarMenu as parent component */}
               <LanguageSwitch />
               <ThemeSwitch />
             </Menubar>
-            <div className="flex gap-3 items-center">
-              <LoginLink
-                className={buttonVariants({ variant: "ghost", size: "sm" })}
-              >
-                {i18n("navbar.auth.sign-in")}
-              </LoginLink>
-              <RegisterLink className={buttonVariants({ size: "sm" })}>
-                {i18n("navbar.auth.sign-up")}
-              </RegisterLink>
-            </div>
+            {user ? (
+              <div className="flex gap-3 items-center">
+                <LogoutLink
+                  className={buttonVariants({ variant: "ghost", size: "sm" })}
+                >
+                  {i18n("navbar.auth.log-out")}
+                </LogoutLink>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({ size: "sm" })}
+                >
+                  {i18n("navbar.dashboard")}
+                </Link>
+              </div>
+            ) : (
+              <div className="flex gap-3 items-center">
+                <LoginLink
+                  className={buttonVariants({ variant: "ghost", size: "sm" })}
+                >
+                  {i18n("navbar.auth.sign-in")}
+                </LoginLink>
+                <RegisterLink className={buttonVariants({ size: "sm" })}>
+                  {i18n("navbar.auth.sign-up")}
+                </RegisterLink>
+              </div>
+            )}
           </div>
         </div>
       </Container>
