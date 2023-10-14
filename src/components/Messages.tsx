121edc4aa7227d0cd70a61b774663b4a22510db9
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-14 11:24:01 +0300

    Added Messages component

---
+++ src/components/Messages.tsx
@@ -0,0 +1,103 @@
+import Message from "./Message";
+import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
+import { Icons } from "./ui/icons";
+import { Skeleton } from "./ui/skeleton";
+import { useContext, useEffect, useRef } from "react";
+import { ChatContext } from "./ChatContext";
+import { useIntersection } from "@mantine/hooks";
+
+interface MessagesProps {
+  fileId: string;
+}
+
+export default function Messages({ fileId }: MessagesProps) {
+  const { isLoading: isAILoading } = useContext(ChatContext);
+
+  const { data, isLoading, fetchNextPage } =
+    trpc.getFileMessages.useInfiniteQuery(
+      {
+        fileId,
+        limit: INFINITE_QUERY_LIMIT,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage?.nextCursor,
+        keepPreviousData: true,
+      }
+    );
+
+  const messages = data?.pages.flatMap((page) => page.messages);
+
+  const loadingMessage = {
+    createdAt: new Date().toISOString(),
+    id: "loading-message",
+    isUserMessage: false,
+    text: (
+      <span className="flex h-full items-center justify-center">
+        <Icons.spinner className="h-4 w-4 animate-spin" />
+      </span>
+    ),
+  };
+
+  const combinedMessages = [
+    ...(isAILoading ? [loadingMessage] : []),
+    ...(messages ?? []),
+  ];
+
+  const lastMessageRef = useRef<HTMLDivElement>(null);
+  const { ref, entry } = useIntersection({
+    root: lastMessageRef.current,
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
+
+  return (
+    <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-border flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-primary scrollbar-thumb-rounded scrollbar-track-lighter scrollbar-w-2 scrolling-touch">
+      {combinedMessages && combinedMessages.length > 0 ? (
+        combinedMessages.map((message, index) => {
+          const isNextMessageSamePerson =
+            combinedMessages[index - 1]?.isUserMessage ===
+            combinedMessages[index]?.isUserMessage;
+          if (index === combinedMessages.length - 1) {
+            return (
+              <Message
+                ref={ref}
+                key={message.id}
+                message={message}
+                isNextMessageSamePerson={isNextMessageSamePerson}
+              />
+            );
+          } else {
+            return (
+              <Message
+                key={message.id}
+                message={message}
+                isNextMessageSamePerson={isNextMessageSamePerson}
+              />
+            );
+          }
+        })
+      ) : isLoading ? (
+        <div className="w-full flex flex-col gap-2">
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+        </div>
+      ) : (
+        <div className="flex flex-1 flex-col items-center justify-center gap-2">
+          <Icons.chat className="h-8 w-8 text-primary/70" />
+          <h3 className="font-semibold text-xl">You're all set!</h3>
+          <p className="text-foreground/70 text-sm">
+            Ask your first question to get started.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
