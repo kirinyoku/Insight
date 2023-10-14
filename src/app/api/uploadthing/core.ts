abadda40225758531d23bed68d8d5da4ef905c6b
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-14 10:57:41 +0300

    Added file vectorization
    After the file is added to the Planetscale database, it will be vectorized and added to the Pinecone database.

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,10 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 
 const f = createUploadthing();
 
@@ -20,15 +24,76 @@ export const ourFileRouter = {
       return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
+      // Adding a PDF file to the database
       const createdFile = await db.file.create({
         data: {
           key: file.key,
           name: file.name,
           userId: metadata.userId,
+          // URL of the S3 bucket where the PDF file is stored
           url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          // "PROCESSING" means that the PDF has been added to the database but not yet vectorized.
           uploadStatus: "PROCESSING",
         },
       });
+
+      // Vectorize a PDF file and save it to the Pinecone vector database.
+      try {
+        // Fetches a file from an S3 bucket
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        );
+
+        // Loads the fetched file as a PDF
+        const blob = await response.blob();
+        const loader = new PDFLoader(blob);
+        const pageLevelDocs = await loader.load(); // Array containing individual pages of a document
+        const pagesAmount = pageLevelDocs.length;
+
+        // Initializes Pinecone, a vector database.
+        const pinecone = new Pinecone({
+          apiKey: process.env.PINECONE_API_KEY!,
+          environment: "gcp-starter",
+        });
+        const pineconeIndex = pinecone.Index("insight");
+
+        // Initializes OpenAI embeddings with an API key
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        // Stores the vectorized documents in Pinecone
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          /* 
+            There should be a namespace property with file.key data here, 
+            but the "Starter" Pinecone plan does not support it.
+            -----------------------------------------------------------
+            namespace: file.key
+          */
+          pineconeIndex,
+        });
+
+        // Updates the upload status of the file in the database to "SUCCESS"
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (error) {
+        // Error handling.
+        console.error(error);
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
