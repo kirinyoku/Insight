34de2ddc6d9d523834d99b205ff8359fb96c165b
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-19 10:59:36 +0300

    Added footer for landing page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,43 @@
+import { useTranslations } from "next-intl";
+import Container from "./layout/Container";
+import { Icons } from "./ui/icons";
+
+export default function Footer() {
+  const i18n = useTranslations("Index");
+
+  return (
+    <footer className="w-full h-16 bg-background/50 border-t border-border backdrop-blur-lg shadow-primary/10 shadow-inner transition-all">
+      <Container>
+        <div className="flex justify-between items-center text-center md:text-left h-16 gap-4">
+          <p className="text-foreground/70">
+            © 2023 {i18n("footer.design")}
+            <a
+              href="https://github.com/joschan21"
+              target="_blank"
+              className="text-primary hover:underline"
+            >
+              Josh
+            </a>
+            , {i18n("footer.development")}
+            <a
+              href="https://github.com/kirinyoku"
+              target="_blank"
+              className="text-primary hover:underline"
+            >
+              Kirin
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://github.com/kirinyoku/Insight"
+              target="_blank"
+              className="flex items-center gap-2 hover:text-primary"
+            >
+              <Icons.gitHub className="h-6 w-6" />
+            </a>
+          </p>
+        </div>
+      </Container>
+    </footer>
+  );
+}
