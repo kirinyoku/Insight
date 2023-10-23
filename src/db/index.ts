8585a77cc4de2a4c1b0a7a50aa68b1892c94ac4c
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-10-23 21:53:04 +0300

    Attempting to fix bug with messages on edge runtime

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,30 +1,18 @@
-import { PrismaClient } from "@prisma/client";
-/* 
-  In production, it creates a new instance for every request. In development,
-  it checks if there’s a cached instance of PrismaClient and uses that. 
-  If not, it creates a new one and caches it. This is done to prevent 
-  too many instances of PrismaClient in development and potentially reaching 
-  the database connection limit. The instance is then exported as db for use 
-  in other parts of your application.
-*/
-declare global {
-  // eslint-disable-next-line no-var
-  var cachedPrisma: PrismaClient; // var for global scope
-}
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-let prisma: PrismaClient;
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate());
+};
 
-if (process.env.NODE_ENV === "production") {
-  // If it's production, create a new instance of PrismaClient
-  prisma = new PrismaClient();
-} else {
-  // Check if there's a cached instance of PrismaClient
-  if (!global.cachedPrisma) {
-    // If there's no cached instance, create a new one and cache it
-    global.cachedPrisma = new PrismaClient();
-  }
-  // Use the cached instance of PrismaClient
-  prisma = global.cachedPrisma;
-}
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export const db = prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,9 +1,11 @@
-import OpenAI from "openai";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { Configuration, OpenAIApi } from "openai-edge";
 
-export const openai = new OpenAI({
+const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+export const openai = new OpenAIApi(config);
 
 // Initialize OpenAI embeddings generator.
 export const embeddings = new OpenAIEmbeddings({
